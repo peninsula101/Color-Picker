ad9034b3391b79d7ac2169d8c4891044db6e966f
TVSCSK <108569220+peninsula101@users.noreply.github.com>
2023-09-20 13:10:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let backGround = document.getElementById("colorPickerContainer");
+let backGroundColorDisplay = document.getElementById("selectedColorHexCode");
+
+function button1() {
+    backGround.style.backgroundColor = "#e0e0e0";
+    backGroundColorDisplay.textContent = "#e0e0e0";
+}
+
+function button2() {
+    backGround.style.backgroundColor = "#6fcf97";
+    backGroundColorDisplay.textContent = "#6fcf97";
+}
+
+function button3() {
+    backGround.style.backgroundColor = "#56ccf2";
+    backGroundColorDisplay.textContent = "#56ccf2";
+}
+
+function button4() {
+    backGround.style.backgroundColor = "#bb6bd9";
+    backGroundColorDisplay.textContent = "#bb6bd9";
+}
